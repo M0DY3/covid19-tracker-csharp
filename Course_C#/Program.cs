e720d8ed6c15d37d3d4f9025431ac6c5ff6c7e80
M0DY3 <moh9238mmoohh@gmail.com>
2025-08-14 13:34:09 +0300

    Added latest 3 updates feature to COVID tracker

--- Course_C#/Program.cs
+++ Course_C#/Program.cs
@@ -19,7 +19,8 @@ namespace CovidTrackerSimple
                 Console.WriteLine("1. Global Stats");
                 Console.WriteLine("2. Country Stats");
                 Console.WriteLine("3. Compare Two Countries");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("4. Global Historical Data");
+                Console.WriteLine("5. Exit");
                 Console.Write("Choose an option: ");
                 input = Console.ReadLine();
 
@@ -38,6 +39,10 @@ namespace CovidTrackerSimple
                         break;
 
                     case "4":
+                        await ShowLatestUpdates(client);
+                        break;
+
+                    case "5":
                         Console.WriteLine("Exiting program...");
                         return;
 
@@ -98,5 +103,37 @@ namespace CovidTrackerSimple
             Console.WriteLine($"{country1} - Cases: {data1["cases"]}, Deaths: {data1["deaths"]}, Recovered: {data1["recovered"]}");
             Console.WriteLine($"{country2} - Cases: {data2["cases"]}, Deaths: {data2["deaths"]}, Recovered: {data2["recovered"]}");
         }
+
+        static async Task ShowLatestUpdates(HttpClient client)
+        {
+            string url = "https://disease.sh/v3/covid-19/historical/all?lastdays=all";
+            string response = await client.GetStringAsync(url);
+            var data = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, long>>>(response);
+
+            Console.WriteLine("\n--- Latest 3 Updates ---");
+
+            foreach (var category in new[] { "cases", "deaths", "recovered" })
+            {
+                Console.WriteLine($"\n({category})");
+
+                // Sort dates
+                var sorted = data[category].OrderBy(entry => DateTime.Parse(entry.Key)).ToList();
+
+                // Take last 4 dates to calculate last 3 updates
+                var lastDates = sorted.Skip(sorted.Count - 4).ToList();
+
+                for (int i = 1; i < lastDates.Count; i++)
+                {
+                    string date = lastDates[i].Key;
+                    long update = lastDates[i].Value - lastDates[i - 1].Value;
+                    Console.WriteLine($"{date}\t+{update}");
+                }
+            }
+        }
+
     }
+
+
+
 }
+
